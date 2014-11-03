e107730344208d5ba5d68f1058ecb4465898d97e
mattroz <mattroz@github.com>
2014-11-03 14:01:07 +0300

    Add a full-title helper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sample::Application.config.secret_key_base = 'eff4f85250188486235b69e0f59b4447454a44f5ae41817b83531f6274f2611b7dc80a5ea979ab2ed3e44accd1ecc53ad23e7e382084009ebe7dae103ebd4557'
