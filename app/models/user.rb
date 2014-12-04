64f44d4c0c1fde72f31303480f571a92effc5127
mattroz <matveyrozanov@gmail.com>
2014-12-04 19:51:16 +0300

    Add comments to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 
+	#we shouldnt have blank names with length bigger than 50 symbols
 	validates :name, presence: true, length: {maximum: 50}
+	
+	#regex for validating user email
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	#we shouldnt have blank email with invalid format and not unique
 	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, 
 			          uniqueness: {case_sensitive: false}
 	validates :password, length: { minimum: 6 }
 
+	#user's password will crypt by BCrypt
     has_secure_password
 end
 
