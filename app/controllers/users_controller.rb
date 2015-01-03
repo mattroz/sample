f88411af55cc24b81573fbbacd99aa8975b9ff53
mattroz <matveyrozanov@gmail.com>
2015-01-04 00:53:03 +0300

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,71 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+ before_action :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-  def new
-  	@user = User.new
-  end
+ def following
+  @title = "Following"
+  @user = User.find(params[:id])
+  @users = @user.followed_users.paginate(page: params[:page])
+  render 'show_follow'
+end
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
+def followers
+  @title = "Followers"
+  @user = User.find(params[:id])
+  @users = @user.followers.paginate(page: params[:page])
+  render 'show_follow'
+end
 
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url
-  end
+def feed
+  Micropost.from_users_followed_by(self)
+end
 
-  def show
-  	@user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
-  end
+def new
+	@user = User.new
+end
 
-   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the @Microblog!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
+def index
+  @users = User.paginate(page: params[:page])
+end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+def destroy
+  User.find(params[:id]).destroy
+  flash[:success] = "User deleted."
+  redirect_to users_url
+end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      # Handle a successful update.
-      flash[:success] = "Profile updated"
-      redirect_to @user
-    else
-      render 'edit'
-    end
+def show
+	@user = User.find(params[:id])
+  @microposts = @user.microposts.paginate(page: params[:page])
+end
+
+ def create
+  @user = User.new(user_params)
+  if @user.save
+    sign_in @user
+    flash[:success] = "Welcome to the @Microblog!"
+    redirect_to @user
+  else
+    render 'new'
   end
+end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+    # Handle a successful update.
+    flash[:success] = "Profile updated"
+    redirect_to @user
+  else
+    render 'edit'
+  end
+end
 
   private
 
